91e5e5bd34790f4e3d061a1ab90e28250529f9ba
Nicholas <nragland37@gmail.com>
2022-02-01 05:43:31 -0600

    read/write string array ~ not finished

--- Assignment 2/assignment_2.2.cpp
+++ Assignment 2/assignment_2.2.cpp
@@ -11,233 +11,111 @@
 //
 //          Due:                  February 7, 2021
 //
-//          This program reads and displays eight test scores, as a percentage, for ten students 
+//          This program reads and displays eight test scores, as a percentage, for ten students
 //          from a file. The functions calculate total points of one student for all tests,
-//          total points for one test for all students, total average of all students for one test, 
-//          average of all tests for one student, highest test score for one student, and the student 
-//          with the highest average test score.       
+//          total points for one test for all students, total average of all students for one test,
+//          average of all tests for one student, highest test score for one student, and the student
+//          with the highest average test score.
 //
 //****************************************************************************************************
 
 #include <iostream>
-#include <iomanip>
+#include <string>
 #include <fstream>
 
 using namespace std;
 
-const int NUM_TESTS = 8;
-
-void readTestScores(ifstream &f, int scores[][NUM_TESTS], int numStds);
-void displayTestScores(const int scores[][NUM_TESTS], int numStds);
-int totalOneTest(const int scores[][NUM_TESTS], int numStds, int testNumber);
-int totalOneStudent(const int scores[][NUM_TESTS], int numStds, int studentNumber);
-double averageOneTest(const int scores[][NUM_TESTS], int numStds, int testNumber);
-double averageOneStudent(const int scores[][NUM_TESTS], int numStds, int studentNumber);
-int highestOneStudent(const int scores[][NUM_TESTS], int numStds, int studentNumber);
-int topStudent(const int scores[][NUM_TESTS], int numStds);
+void readNames(ifstream &inputFile, string names[], int numNames);
+// Read all names from the file. The names should be stored in the array as they are stored in the file.
+void displayNames(const string names[], int numNames);
+// Display the names.
+int searchNames(const string names[], int numNames, const string &name);
+/*Return the array index if the name, passed as the third argument, is found in the
+array.  If the name is not found then return -1.  The user should be prompted for
+the name before the function is called.
+Use the ‘linear search’ algorithm to implement this function.*/
+void bubbleSort(string names[], int numNames);
+// Sort the names in ascending order using the bubble sort algorithm.
+void bubbleSortDescending(string names[], int numNames);
+// Sort the names in descending order using the bubble sort algorithm
 
 //****************************************************************************************************
 
 int main()
 {
-    ifstream f("Assignment1Data.txt");
+    ifstream inputFile("StudentNames.txt");
 
-    const int NUM_STDS = 10;
+    const int NUM_NAMES = 21;
 
-    int scores[NUM_STDS][NUM_TESTS];
+    string names [NUM_NAMES];
 
-    readTestScores(f, scores, NUM_STDS);
-    displayTestScores(scores, NUM_STDS);
-
-    cout << setprecision(2) << fixed;
-
-    cin.get();
-    cout << "\nTotal for Test #1 is: " << totalOneTest(scores, NUM_STDS, 0);
-    cin.get();
-    cout << "\n\nTotal for Test #5 is: " << totalOneTest(scores, NUM_STDS, 4);
-    cin.get();
-    cout << "\n\nTotal for Student #3 is: " << totalOneStudent(scores, NUM_STDS, 2);
-    cin.get();
-    cout << "\n\nTotal for Student #8 is: " << totalOneStudent(scores, NUM_STDS, 7);
-    cin.get();
-    cout << "\n\nAverage for Test #5: " << averageOneTest(scores, NUM_STDS, 4);
-    cin.get();
-    cout << "\n\nAverage for Test #8: " << averageOneTest(scores, NUM_STDS, 7);
-    cin.get();
-    cout << "\n\nAverage for Student #5: " << averageOneStudent(scores, NUM_STDS, 4);
-    cin.get();
-    cout << "\n\nAverage for Student #9: " << averageOneStudent(scores, NUM_STDS, 8);
-    cin.get();
-    cout << "\n\nHighest test score for Student #2: " << highestOneStudent(scores, NUM_STDS, 1);
-    cin.get();
-    cout <<"\n\nThe top Student: " << topStudent(scores, NUM_STDS);
-    cin.get();
-
-    f.close();
+    readNames(inputFile, names, NUM_NAMES);
+    displayNames(names, NUM_NAMES);
     
-	return 0;
+    inputFile.close();
+
+    return 0;
 }
 
 //****************************************************************************************************
 
-void readTestScores(ifstream &f, int scores[][NUM_TESTS], int numStds)
+void readNames(ifstream &inputFile, string names[], int numNames)
 {
-    if (f.is_open())
-        for (int studentNumber = 0; studentNumber < numStds; ++studentNumber) {
-            for (int testNumber = 0; testNumber < NUM_TESTS; ++testNumber) {
-                f >> scores[studentNumber][testNumber];
-            }
+    if (inputFile.is_open())
+        for (int nameNumber = 0; nameNumber < numNames; ++nameNumber)
+        {
+             inputFile >> names[nameNumber];
         }
     else
-    cout << "ERROR: Could not open file" << endl; 
+        cout << "ERROR: Could not open file" << endl;
 }
 
 //****************************************************************************************************
 
-void displayTestScores(const int scores[][NUM_TESTS], int numStds)
+void displayNames(const string names[], int numNames)
 {
-    cout << "All Test scores:" << endl;
-    
-    for (int studentNumber = 0; studentNumber < numStds; ++studentNumber) {
-         for (int testNumber = 0; testNumber < NUM_TESTS; ++testNumber) {
-             cout << scores[studentNumber][testNumber] << " ";
-        }
-        cout << endl;
+    cout << "All Names:" << endl;
+    for (int nameNumber = 0; nameNumber < numNames; ++nameNumber)
+    {
+        cout << names[nameNumber] << " " << endl;
     }
 }
 
 //****************************************************************************************************
 
-int totalOneTest(const int scores[][NUM_TESTS], int numStds, int testNumber)
+int searchNames(const string names[], int numNames, const string &name)
 {
-    int sum = 0;
 
-    for (int studentNumber = 0; studentNumber < numStds; ++studentNumber) {
-        sum += scores[studentNumber][testNumber];
-    }
-
-    return sum;
+    return 0;
 }
 
 //****************************************************************************************************
 
-int totalOneStudent(const int scores[][NUM_TESTS], int numStds, int studentNumber)
+void bubbleSort(string names[], int numNames)
 {
-    int sum = 0;
 
-    for (int testNumber = 0; testNumber < NUM_TESTS; ++testNumber) {
-        sum += scores[studentNumber][testNumber];
-    }
-
-    return sum;
 }
 
 //****************************************************************************************************
 
-double averageOneTest(const int scores[][NUM_TESTS], int numStds, int testNumber)
-{   
-    int total = 0;
-    double average;
-
-    for (int studentNumber = 0; studentNumber < numStds; ++studentNumber) {
-        total += scores[studentNumber][testNumber];
-    }
-    average = static_cast<double>(total) / numStds;
-
-    return average;
-}
-
-//****************************************************************************************************
-
-double averageOneStudent(const int scores[][NUM_TESTS], int numStds, int studentNumber)
+void bubbleSortDescending(string names[], int numNames)
 {
-    int total = 0;
-    double average;
 
-    for (int testNumber = 0; testNumber < NUM_TESTS; ++testNumber) {
-        total += scores[studentNumber][testNumber];
-    }
-    average = static_cast<double>(total) / NUM_TESTS;
-
-    return average;
-}
-
-//****************************************************************************************************
-
-int highestOneStudent(const int scores[][NUM_TESTS], int numStds, int studentNumber)
-{
-    int highest = scores[studentNumber][0];
-    for (int testNumber = 1; testNumber < NUM_TESTS; ++testNumber) {
-        if (scores[studentNumber][testNumber] > highest) {
-            highest = scores[studentNumber][testNumber];
-        }
-    }
-    return highest;
-}
-
-//****************************************************************************************************
-
-int topStudent(const int scores[][NUM_TESTS], int numStds)            
-{
-    int topPercent = 0,
-        topStudent = 0,
-        current;
-
-    for (int studentNumber = 0; studentNumber < numStds; ++studentNumber) {
-        current = averageOneStudent(scores, numStds, studentNumber);
-        if (current > topPercent) {
-            topPercent = current;
-            topStudent = studentNumber;
-        }
-    }
-
-    return topStudent+1;
 }
 
 /*
 
-All Test scores:
-87 90 65 45 88 89 75 80
-89 85 78 56 90 91 99 82
-80 90 72 95 99 66 77 88
-90 65 80 98 99 76 77 88
-55 75 66 99 99 56 87 86
-30 60 80 90 99 56 67 88
-90 55 95 35 98 86 37 88
-99 97 99 98 98 96 87 83
-90 65 80 98 99 76 87 88
-35 75 60 99 99 56 87 88
+All names:
+Smith, John
+Song, Mona
+Jones, Trevor
+Li, Na
+Zhang, Xiu Ying
+Saleem, Mohammad
+Lloyd, Arthur
+Jones, Rhys
+Evans, Olivia
+Davies, Emily
 
-Press Enter to Continue 
- 
-Total for Test #1 is: 745
-Press Enter to Continue 
- 
-Total for Test #5 is: 968
-Press Enter to Continue 
- 
-Total for Student #3 is: 667 
-Press Enter to Continue 
- 
-Total for Student #8 is: 757
-Press Enter to Continue 
- 
-Average for Test #5: 96.80  
-Press Enter to Continue 
- 
-Average for Test #8: 85.90 
-Press Enter to Continue 
- 
-Average for Student #5: 77.88
-Press Enter to Continue 
- 
-Average for Student #9: 85.38 
-Press Enter to Continue 
- 
-Highest test score for Student #2: 99 
-Press Enter to Continue 
- 
-The top Student: 8 
-Press Enter to Continue
 
 */
