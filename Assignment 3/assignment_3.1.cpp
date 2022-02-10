2a93fab43c4a5c9d2c010ce61966a3e53a799445
Nicholas <nragland37@gmail.com>
2022-02-09 18:16:48 -0600

    fnished 3.1 - need review

--- Assignment 3/assignment_3.1.cpp
+++ Assignment 3/assignment_3.1.cpp
@@ -1,5 +1,5 @@
 //****************************************************************************************************
-// 			File:                 assignment_2.2.cpp
+// 			File:                 assignment_3.1.cpp
 //
 //			Student:              Nicholas Ragland
 //
@@ -12,10 +12,16 @@
 //          Due:                  February 14, 2022
 //
 //          This program reads and displays ten student names and their places in a list from a file.
-//          Prompts the user to enter a name then searches the list to find if name is present.
-//          If name is found, the program outputs the students place in the list. If name is not
-//          found, the program outputs -1. The program then outputs two seperate lists of an ascending
-//          and descending order with the students place in both.
+//          Prompts the user to enter a name and searches (Linear Search Algorithm) the list to find 
+//          if name is present. If name is found, the program outputs the students place in the list. 
+//          If name is not found, the program outputs -1. The program outputs two seperate lists 
+//          of an ascending and descending order using a "Bubble Sort Algorithm" with the students 
+//          place in both. Then the program sorts the list in ascending order using a "Selection Sort 
+//          Algorithm", prompts the user to enter a name, and searches (Binary Search Algorithm) the 
+//          list to find if name is present. If name is found, the program outputs the students place 
+//          in the list. If name is not found, the program outputs -1. The program outputs another 
+//          list in descenidng order using the "Selection Sort Algorithm" and repeats the process of 
+//          the "Binary Search Algorithm".   
 //
 //****************************************************************************************************
 
@@ -29,11 +35,12 @@ using namespace std;
 void readNames(ifstream &inputFile, string names[], int numNames);
 void displayNames(const string names[], int numNames);
 int linearSearchNames(const string names[], int numNames, const string &name);
-void displaySearchNames(const string names[], int numNames, const string &name);
+void displayLinearSearchNames(const string names[], int numNames, const string &name);
 void swapValues(string &a, string &b);
 void bubbleSort(string names[], int numNames);
 void bubbleSortDescending(string names[], int numNames);
 int binarySearchNames(const string names[], int numNames, const string &name);
+void displayBinarySearchNames(const string names[], int numNames, const string &name);
 void selectionSort(string names[], int numNames);
 void selectionSortDescending(string names[], int numNames);
 
@@ -58,7 +65,7 @@ int main()
     cout << "Enter Student name (e.g., first name, last name): " << endl;
     getline(cin, name);
     linearSearchNames(names, NUM_NAMES, name);
-    displaySearchNames(names, NUM_NAMES, name);
+    displayLinearSearchNames(names, NUM_NAMES, name);
     cin.get();
 
     cout << "\n---------------------------------" << endl;
@@ -67,7 +74,7 @@ int main()
     bubbleSort(names, NUM_NAMES);
     displayNames(names, NUM_NAMES);
     linearSearchNames(names, NUM_NAMES, name);
-    displaySearchNames(names, NUM_NAMES, name);
+    displayLinearSearchNames(names, NUM_NAMES, name);
     cin.get();
 
     cout << "\n---------------------------------" << endl;
@@ -76,10 +83,30 @@ int main()
     bubbleSortDescending(names, NUM_NAMES);
     displayNames(names, NUM_NAMES);
     linearSearchNames(names, NUM_NAMES, name);
-    displaySearchNames(names, NUM_NAMES, name);
+    displayLinearSearchNames(names, NUM_NAMES, name);
     cin.get();
 
+    selectionSort(names, NUM_NAMES);
+    cout << "\n---------------------------------" << endl;
+    cout << setw(32) << "Student Names ~ Ascending Order" << endl;
+    cout << "---------------------------------" << endl;
     displayNames(names, NUM_NAMES);
+
+    cout << "\nFind another Students place in the ascending list." << endl;
+    cout << "Enter Student name (e.g., first name, last name): " << endl;
+    getline(cin, name);
+    binarySearchNames(names, NUM_NAMES, name);
+    displayBinarySearchNames(names, NUM_NAMES, name);
+    cin.get();
+
+    cout << "\n---------------------------------" << endl;
+    cout << "Student Names ~ Descending Order" << endl;
+    cout << "---------------------------------" << endl;
+    selectionSortDescending(names, NUM_NAMES);
+    displayNames(names, NUM_NAMES);
+    binarySearchNames(names, NUM_NAMES, name);
+    displayBinarySearchNames(names, NUM_NAMES, name);
+    cin.get();
 
     inputFile.close();
 
@@ -199,10 +226,47 @@ void bubbleSortDescending(string names[], int numNames)
 
 int binarySearchNames(const string names[], int numNames, const string &name)
 {
-    return 0;
+    int first = 0;
+    int last = numNames - 1;
+    int middle;
+    int position = -1;
+    bool found = false;
+    while (!found && (first <= last))
+    {
+        middle = (first + last) / 2;
+        if (names[middle] == name)
+        {
+            found = true;
+            position = middle + 1;
+        }
+        else if (names[middle] > name)
+        {
+            last = middle - 1;
+        }
+        else
+        {
+            first = middle + 1;
+        }
+    }
+    return position;
 }
 
 //****************************************************************************************************
+
+void displayBinarySearchNames(const string names[], int numNames, const string &name)
+{
+    if (binarySearchNames(names, numNames, name) == -1)
+    {
+        cout << binarySearchNames(names, numNames, name) << " \nName Not Found" << endl;
+    }
+    else
+    {
+        cout << "\nName Found: " << binarySearchNames(names, numNames, name) << endl;
+    }
+}
+
+//****************************************************************************************************
+
 void selectionSort(string names[], int numNames)
 {
     int minIndex;
@@ -265,9 +329,44 @@ void selectionSortDescending(string names[], int numNames)
 
 Find a Students place in the list.
 Enter Student name (e.g., first name, last name):
-Smith, John
+Nicholas, Ragland
+-1
+Name Not Found
 
-Name Found: 1
+
+---------------------------------
+ Student Names ~ Ascending Order
+---------------------------------
+        1  Davies, Emily
+        2  Evans, Olivia
+        3  Jones, Rhys
+        4  Jones, Trevor
+        5  Li, Na
+        6  Lloyd, Arthur
+        7  Saleem, Mohammad
+        8  Smith, John
+        9  Song, Mona
+        10  Zhang, Xiu Ying
+-1
+Name Not Found
+
+
+---------------------------------
+Student Names ~ Descending Order
+---------------------------------
+        1  Zhang, Xiu Ying
+        2  Song, Mona
+        3  Smith, John
+        4  Saleem, Mohammad
+        5  Lloyd, Arthur
+        6  Li, Na
+        7  Jones, Trevor
+        8  Jones, Rhys
+        9  Evans, Olivia
+        10  Davies, Emily
+-1
+Name Not Found
+
 
 ---------------------------------
  Student Names ~ Ascending Order
@@ -283,23 +382,28 @@ Name Found: 1
         9  Song, Mona
         10  Zhang, Xiu Ying
 
-Name Found: 8
+Find another Students place in the ascending list.
+Enter Student name (e.g., first name, last name):
+Nicholas, Ragland
+-1
+Name Not Found
+
 
 ---------------------------------
 Student Names ~ Descending Order
 ---------------------------------
-        1  Zhang, Xiu Ying
-        2  Song, Mona
-        3  Smith, John
-        4  Saleem, Mohammad
-        5  Lloyd, Arthur
-        6  Li, Na
-        7  Jones, Trevor
-        8  Jones, Rhys
-        9  Evans, Olivia
-        10  Davies, Emily
-
-Name Found: 3
+        1  Davies, Emily
+        2  Evans, Olivia
+        3  Jones, Rhys
+        4  Jones, Trevor
+        5  Li, Na
+        6  Lloyd, Arthur
+        7  Saleem, Mohammad
+        8  Smith, John
+        9  Song, Mona
+        10  Zhang, Xiu Ying
+-1
+Name Not Found
 
 //****************************************************************************************************
 
@@ -319,9 +423,10 @@ Name Found: 3
 
 Find a Students place in the list.
 Enter Student name (e.g., first name, last name):
-Nicholas, Ragland
--1
-Name Not Found
+Smith, John
+
+Name Found: 1
+
 
 ---------------------------------
  Student Names ~ Ascending Order
@@ -336,8 +441,9 @@ Name Not Found
         8  Smith, John
         9  Song, Mona
         10  Zhang, Xiu Ying
--1
-Name Not Found
+
+Name Found: 8
+
 
 ---------------------------------
 Student Names ~ Descending Order
@@ -352,7 +458,45 @@ Student Names ~ Descending Order
         8  Jones, Rhys
         9  Evans, Olivia
         10  Davies, Emily
--1
-Name Not Found
+
+Name Found: 3
+
+
+---------------------------------
+ Student Names ~ Ascending Order
+---------------------------------
+        1  Davies, Emily
+        2  Evans, Olivia
+        3  Jones, Rhys
+        4  Jones, Trevor
+        5  Li, Na
+        6  Lloyd, Arthur
+        7  Saleem, Mohammad
+        8  Smith, John
+        9  Song, Mona
+        10  Zhang, Xiu Ying
+
+Find another Students place in the ascending list.
+Enter Student name (e.g., first name, last name):
+Li, Na
+
+Name Found: 5
+
+
+---------------------------------
+Student Names ~ Descending Order
+---------------------------------
+        1  Davies, Emily
+        2  Evans, Olivia
+        3  Jones, Rhys
+        4  Jones, Trevor
+        5  Li, Na
+        6  Lloyd, Arthur
+        7  Saleem, Mohammad
+        8  Smith, John
+        9  Song, Mona
+        10  Zhang, Xiu Ying
+
+Name Found: 5
 
 */
