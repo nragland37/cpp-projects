0e47ecd1bc4ea18c2ea0622dd2cc4869e3bffc23
Félix Larose-Gervais <filedesless@gmail.com>
2022-02-18 11:12:39 -0500

    impl rev_array

--- bombies/rev_array.cpp
+++ bombies/rev_array.cpp
@@ -4,40 +4,30 @@ using namespace std;
 
 const int LIST_LEN = 10;
 
-void print_array_backwards(int list[LIST_LEN]) // reverses the order but not array itself 
-{
-    for (int i = LIST_LEN-1; i >= 0; --i){
+void print_array(int list[LIST_LEN]) {
+    for (int i = 0; i < LIST_LEN; ++i) {
         cout << list[i] << " ";
     }
 }
 
-void print_array(int list[LIST_LEN]) 
-{
-    for (int i = 0; i < LIST_LEN; ++i){
-        cout << list[i] << " ";
-    }
+void swap(int list[LIST_LEN], int i, int j) {
+    int temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
 }
 
-// TODO: swap the array with temporary element 
-void swap_array(int list[LIST_LEN], int x, int y) 
-{
-    int temp = list[x];
-    list[x] = list[y];
-    list[y] = temp;
-}
-
-int main()
-{
+int main() {
     int list[LIST_LEN] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
     cout << "Initial array: ";
     print_array(list);
+    cout << endl;
 
-    cout << endl << "Reverse values of array: "; 
-    print_array_backwards(list);
+    for (int i = 0; i < LIST_LEN / 2; ++i) {
+        swap(list, i, LIST_LEN - i - 1);
+    }
 
-    cout << endl << "Reversed_array: ";
-    swap_array(list, 9, 0);
+    cout << "Reversed_array: ";
     print_array(list);
-
     cout << endl;
 }
