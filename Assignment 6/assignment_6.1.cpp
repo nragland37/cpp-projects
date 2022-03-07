5dc914c9dea955c15531d27580359b6360549f0f
Nicholas <nragland37@gmail.com>
2022-03-06 23:13:23 -0600

    added a few user - edge case preventions

--- Assignment 6/assignment_6.1.cpp
+++ Assignment 6/assignment_6.1.cpp
@@ -23,16 +23,17 @@ int numberWords(const char* words, int size);
 
 int main() {
     const int SIZE = 50;
-    int result;
+    int result,
+        numInput = 3;
 
     char* words = new char[SIZE];
 
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < numInput; ++i) {
         cout << "Enter a line of words (less than 50 characters), ended by '.': " << endl;
         cin.getline(words, SIZE);
         result = numberWords(words, SIZE);
-        cout << "Total words: " << result << endl;
-        cin.get();
+        cout << "Total words: " << result << endl
+             << endl;
     }
 
     delete[] words;
@@ -47,8 +48,12 @@ int numberWords(const char* words, int size) {
     int total = 0;
     char lastChar = '\0';
 
-    if (words[0] == '\0') {  //  PROBLEMS: space before first word(+1) / space after last word(+1) / only ',' or ' ' (+1)
-        return 0;            //  IDEAS: work with isalpha()
+    if (words[0] == '\0') {  // user inputs no words
+        return 0;
+    }
+
+    if (words[0] == ' ' || words[0] == ',') {  // user inputs a space/comma first
+        total--;
     }
 
     for (int i = 0; i < size; ++i) {
@@ -59,6 +64,9 @@ int numberWords(const char* words, int size) {
             }
         }
         lastChar = words[i];
+        if (words[i] == '.' && words[i + 1] == '\0') {  // stops loop with '.' and followed by null byte [incase titles (mr.,ms.,etc)]
+            break;
+        }
     }
 
     return total + 1;
@@ -68,14 +76,28 @@ int numberWords(const char* words, int size) {
 
 Enter a line of words (less than 50 characters), ended by '.':
 
-Total words: 0
+Total words: 0                                                      // user inputs no words
 
-Enter a line of words (less than 50 characters), ended by '.': 
+Enter a line of words (less than 50 characters), ended by '.':
 This contains a name,address, and phone number.
 Total words: 8
 
-Enter a line of words (less than 50 characters), ended by '.': 
-This contains a name,,,,,address,     and phone number.
-Total words: 8
+Enter a line of words (less than 50 characters), ended by '.':
+This,,,,contains,,,, a    name.
+Total words: 4
+
+//****************************************************************************************************
+
+Enter a line of words (less than 50 characters), ended by '.':
+,,,,,,hi.
+Total words: 1                                                       // user inputs a comma first
+
+Enter a line of words (less than 50 characters), ended by '.':      // user inputs titles (mr.,ms.,etc)]
+Hello, we are Mr. and Mrs. Smith.
+Total words: 7
+
+Enter a line of words (less than 50 characters), ended by '.':
+    Hello,,,, we,  are Mr. and Mrs.,, Smith.                       // user inputs a space first & titles (mr.,ms.,etc)]
+Total words: 7
 
 */
