5156155948f6a28a32492627ee0513e9b991d829
Nicholas <nragland37@gmail.com>
2022-04-05 11:50:31 -0500

    things and stuff. yeppies.

---
+++ Assignment 8/assignment_8.cpp
@@ -0,0 +1,62 @@
+//****************************************************************************************************
+// 		    File:                 assignment_8.cpp
+//
+//		    Student:              Nicholas Ragland
+//
+//		    Assignment:           Program #8
+//
+//          Course Name:          Programming II
+//
+//          Course Number:        COSC-1560 - 02
+//
+//          Due:                  April 11, 2022
+//
+//****************************************************************************************************
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//****************************************************************************************************
+struct Employee {
+    string name;
+    int age;
+};
+
+Employee* readEmployees(const string& empFile, int& numEmps); 
+void displayEmployees(const Employee emps[], int numEmps);
+Employee* inputEmployees(Employee* emps, int& numEmps); 
+
+
+//****************************************************************************************************
+
+int main() {
+
+    return 0;
+}
+
+//****************************************************************************************************
+
+Employee* readEmployees(const string& empFile, int& numEmps) {
+    return;
+}
+
+//****************************************************************************************************
+
+Employee* inputEmployees(Employee* emps, int& numEmps) {
+    return;
+
+}
+
+//****************************************************************************************************
+
+void displayEmployees(const Employee emps[], int numEmps) {
+
+}
+
+/*
+
+
+*/
