035ea5b7443db7f0294b187ebf138a2d665f587c
Nicholas <nragland37@gmail.com>
2022-04-13 22:15:11 -0500

    boop beep

--- Assignment 9/assignment_9.cpp
+++ Assignment 9/assignment_9.cpp
@@ -36,28 +36,76 @@ struct Person {
     Date testTaken;
 };
 
-Translation* readTranslation(const string& filename, int& num);
-Person* readTesters(const string& filename, int& num);
+Translation* readTranslation(const string& fileName, int& numT);
+Person* readTesters(const string& fileName, int& numP);
 void testingOptions(const Translation t[], int numT, Person people[], int numP);
 void takeTest(const Translation t[], int numT, Person& p);
 void displayTesters(const Person p[], int numP);
-void writeTesters(const string& filename, const Person p[], int numP);
+void writeTesters(const string& fileName, const Person p[], int numP);
 
 //****************************************************************************************************
 
 int main() {
+    int numT = 0;
+    int numP = 0;
+    string fileName;
+
+    Translation* t = readTranslation("Translation.txt",num);
+    Person* people = readTesters("Testers.txt",num);
 
     return 0;
 }
 
 //****************************************************************************************************
 
-Translation* readTranslation(const string& filename, int& num) {
+Translation* readTranslation(const string& fileName, int& num) {
+    fstream f;
+    f.open(fileName, ios::in);
+
+    if (f) {
+        f >> num;
+        f.ignore();
+        Translation* t = new Translation[num];
+
+        for (int i = 0; i < num; i++) {
+            getline(f, t[i].american, ',');
+            f.ignore();
+            getline(f, t[i].english);
+            f.ignore();
+        }
+        f.close();
+    } else {
+        cout << "File not found.";
+    }
+    return t;
 }
 
 //****************************************************************************************************
 
-Person* readTesters(const string& filename, int& num) {
+Person* readTesters(const string& fileName, int& num) {
+    fstream f;
+    f.open(fileName, ios::in);
+    if (f) {
+        f >> num;
+        f.ignore();
+        Person* people = new Employee[num];
+
+        for (int i = 0; i < num; i++) {
+            getline(f, people[i].name, ',');
+            f >> people[i].age;
+            f.ignore();
+            f >> people[i].dateEmp.month;
+            f.ignore();
+            f >> people[i].dateEmp.day;
+            f.ignore();
+            f >> people[i].dateEmp.year;
+            f.ignore();
+        }
+        f.close();
+    } else {
+        cout << "File not found.";
+    }
+    return p;
 }
 
 //****************************************************************************************************
@@ -77,7 +125,7 @@ void displayTesters(const Person p[], int numP) {
 
 //****************************************************************************************************
 
-void writeTesters(const string& filename, const Person p[], int numP) {
+void writeTesters(const string& fileName, const Person p[], int numP) {
 }
 
 /*
