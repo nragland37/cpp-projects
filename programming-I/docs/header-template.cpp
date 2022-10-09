c94cb27728790e1d23e417f227c00487e2b3cacd
Nicholas Ragland <nragland37@gmail.com>
2022-10-09 00:07:56 -0500

    added cpp resources

---
+++ programming-I/docs/header-template.cpp
@@ -0,0 +1,16 @@
+//****************************************************************************************************
+// 			File:                 helloWorld.cpp
+//
+//			Student:              John Smith  
+//
+//			Assignment:           Program #1
+//			
+//          Course Name:          Programming 1
+//
+//          Course Number:        COSC 1550 - 03
+//          
+//          Due:                  August 24, 2021
+//
+//          This program outputs the message "hello world!" on the screen.
+//
+//****************************************************************************************************
